0b07cf3c1e983a1268a66353dc00b2bb3b24fe31
hwts <47588515+hwts@users.noreply.github.com>
2023-02-12 16:37:23 +0300

    add debug mod

--- backup.sh
+++ backup.sh
@@ -2,10 +2,8 @@
 
 # vars
 BACKUP_NAME="backup"
-#### full - полный бекап, inc - инкрементный
 BACKUP_MODE="inc"
-#DEBUG_MODE="false" Режим debug будет доступен в след версии скрипта
-#ROTATE="" Режим Logrotate будет доступен в след версии скрипта
+DEBUG_MODE=0
 SSH_KEY="./id_rsa"
 BACKUP_SRC="/files"
 SRC_USER="user"
@@ -22,7 +20,7 @@ BACKUP_INC_OLD_DEST="/mnt/backups/IncOld"
 DATE_NOW="$(date +"%Y-%m-%d_%H-%M")"
 
 ### Ключи и аргументы
-while getopts ":m:b:d:u:n:s:h" arg; do
+while getopts ":m:b:d:u:n:s:vh" arg; do
   case $arg in
     m)
       BACKUP_MODE=$OPTARG
@@ -42,8 +40,11 @@ while getopts ":m:b:d:u:n:s:h" arg; do
     s)
       SRC_SERVER=$OPTARG
       ;;
+    v)
+      DEBUG_MODE=1
+      ;;
     h)
-      echo "Usage: backup.sh [-m backup_mode] [-b source_backup_dir] [-d dest_backup_dir] [-u user] [-n backup_name] [-s server] [-v debug] [-h help]"
+      echo "Usage: backup.sh [-m backup_mode] [-b source_backup_dir] [-d dest_backup_dir] [-u user] [-n backup_name] [-s server] [-v enable_debug] [-h help]"
       exit 0
       ;;
     \?)
@@ -57,8 +58,10 @@ while getopts ":m:b:d:u:n:s:h" arg; do
   esac
 done
 
+if [ "$DEBUG_MODE" == "1" ]; then
+  echo "Проверка корректности переменных"
+fi
 
-### Проверка корректности опций
 if [ "$BACKUP_MODE" != "full" ] && [ "$BACKUP_MODE" != "inc" ]; then
   echo "Error: Некоректное значение, установите тип бекапа full или inc" >&2
   exit 1
@@ -89,6 +92,10 @@ if [ "$SRC_SERVER" == "" ]; then
   exit 1
 fi
 
+if [ "$DEBUG_MODE" == "1" ]; then
+  echo "Создание директорий для бекапа"
+fi
+
 ### Создаем директории если не существуют
 if [ ! -d "$BACKUP_FULL_DEST" ]; then
   mkdir -p  $BACKUP_FULL_DEST
@@ -106,7 +113,9 @@ if [ ! -d "$BACKUP_INC_OLD_DEST" ]; then
   mkdir -p $BACKUP_INC_OLD_DEST
 fi
 
-### backup
+if [ "$DEBUG_MODE" == "1" ]; then
+  echo "Запуск $BACKUP_MODE бекапа"
+fi
 
 if [ "$BACKUP_MODE" == "full" ]; then
 
@@ -129,4 +138,10 @@ if [ "$BACKUP_MODE" == "inc" ]; then
   --link-dest="$BACKUP_DEST" "$SRC_USER"@"$SRC_SERVER":"$BACKUP_SRC" $BACKUP_OLD_DEST/"$BACKUP_NAME"-"$DATE_NOW"
   rm -rf "$BACKUP_DEST"
   ln -s "$BACKUP_OLD_DEST/$BACKUP_NAME-$DATE_NOW" "$BACKUP_DEST"
+fi
+
+if [ $? -eq 0 ]; then
+  echo "Бекап выполнен успешно"
+else
+  echo "Бекап выполнен с ошибками"
 fi
